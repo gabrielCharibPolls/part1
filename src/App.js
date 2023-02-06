1304b16aee74971e1266ef462889b8a612e0a9a0
Gabriel Charib <gabrielcharib@gmail.com>
2023-02-06 15:01:01 +0100

    exercice : 1.5 : step  5

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@
 const Header = (props) => {
   return (
     <div>
-      <h1>{props.course}</h1>
+      <h1>{props.name}</h1>
     </div>
   )
 
@@ -36,30 +36,30 @@ const Total = (props) => {
 
 
 const App = () => {
-
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
 
   return (
     <div>
-      <p>test</p>
-      <Header course={course}/>
-      <Part courseName={parts} />
-      <Total parts={parts}/>
+      <Header course={course.name}/>
+      <Part courseName={course.parts} />
+      <Total parts={course.parts}/>
     </div>
   )
 }
