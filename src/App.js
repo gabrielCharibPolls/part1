d83272c04350c8a83a06983a3730b825dd761b15
Gabriel Charib <gabrielcharib@gmail.com>
2023-02-07 12:16:31 +0100

    status of components exercises

--- src/App.js
+++ src/App.js
@@ -1,72 +1,37 @@
-
-
-
-const Header = (props) => {
-  return (
-    <div>
-      <h1>{props.course}</h1>
-    </div>
-  )
-
-}
-
-
-
-const Part= (props) => {
-  return (
-    <div>
-       <p>{props.course[0].name} {props.course[0].exercises } </p>
-       <p>{props.course[1].name} {props.course[1].exercises } </p>
-       <p>{props.course[2].name}  {props.course[2].exercises } </p>
-    </div>
-  )
-
-}
-
-const Total = (props) => {
-  return (
-    <div>
-      <p> Number of exercises :  {props.exercises1 + props.exercises2 + props.exercises3}</p>
-    </div>
-  )
-
-}
-
-
+import { useState } from 'react'
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
-  }
+  const [ counter, setCounter ] = useState(0)
 
+///////////////////////////////
+//methode compteur + 1
+//////////////////////////////
+const increaseByOne = () => setCounter(counter + 1 )
+const decreaseByOne = () => setCounter(counter - 1)
+const setToZero = () => setCounter(0)
 
+/////////////////////////////////////////
+//methode qui affiche le compteur 
+////////////////////////////////////
+const Display = (props) => {
+return (
+  <div>{props.counter}</div>
+)
+
+}
   return (
     <div>
-      <Header course={course.name}/>
-      <Part course={course.parts} />
-      
-      
+
+    <Display counter={counter} />
+    <button onClick={increaseByOne}> 
+  plus
+</button>
+    <button onClick={setToZero}> 
+        zero
+      </button>
+
     </div>
   )
 }
 
 export default App
-
-
-
-
-
