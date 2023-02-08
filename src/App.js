26aca7eca849dc6596707801af07f78982a624a2
Gabriel Charib <gabrielcharib@gmail.com>
2023-02-08 15:34:41 +0100

    2.6 : phonebook
    small bug in the form

--- src/App.js
+++ src/App.js
@@ -1,20 +1,37 @@
-import { useState } from 'react'
-import Note from './components/Note'
+import React, { useState } from 'react';
 
+const App = () => {
+  const [contacts, setContacts] = useState([
+    { name: 'test', number: '123' },
+    { name: 'test', number: '123' },
+  ]);
 
-const App = (props) => {
-  const [notes, setNotes] = useState(props.notes)
+  const addContact = () => {
+    setContacts([...contacts, { name: 'gabriel', number: 'charib' }]);
+  };
 
   return (
     <div>
-      <h1>Notes</h1>
-      <ul>
-        {notes.map(note => 
-          <Note key={note.id} note={note} />
-        )}
-      </ul>
+      <h2>Phone Directory</h2>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Number</th>
+          </tr>
+        </thead>
+        <tbody>
+          {contacts.map((contact, index) => (
+            <tr key={index}>
+              <td>{contact.name}</td>
+              <td>{contact.number}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <button onClick={addContact}>Add</button>
     </div>
-  )
-}
+  );
+};
 
-export default App 
+export default App
